45c3b4074a9606eeff201963996ca2ccbf427930
laksh-ag <lakshyaagarwal1@gmail.com>
2023-12-28 17:30:54 -0800

    first commit

--- src/main/java/org/example/Api.java
+++ src/main/java/org/example/Api.java
@@ -1,2 +1,12 @@
-package org.example;public class Api {
+package org.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+public class Api {
+    @GetMapping("/user")
+    public void print(){
+        System.out.println();
+    }
 }
